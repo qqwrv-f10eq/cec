207399aa0fe635d719108523acbfc9f69693c8f0
Brian-Less <43507719+Brian-Less@users.noreply.github.com>
2021-10-17 04:06:42 +0700

    Update natives.cpp

--- src/natives.cpp
+++ src/natives.cpp
@@ -10,8 +10,9 @@ The code here acts as the translation between AMX data types and native types.
 #include "natives.hpp"
 
 unsigned char igor_spaces[] = { ' ', '\r', '\n', '\t' };
+unsigned char spaces[] = { ' ', '\r', '\t' };
 
-// This is vowel from Thai language, They will merge to character that will make embed color not work at well
+// This is tone mark and vowel mark from Thai language, It merge normal character with mark. That will make embed color not work at well
 /*
 	0xD1	0x0E31 #THAI CHARACTER MAI HAN-AKAT
 	0xD4	0x0E34 #THAI CHARACTER SARA I
@@ -30,9 +31,38 @@ unsigned char igor_spaces[] = { ' ', '\r', '\n', '\t' };
 	0xED	0x0E4D #THAI CHARACTER NIKHAHIT
 	0xEE	0x0E4E #THAI CHARACTER YAMAKKAN
 	0xD3	0x0E33 #THAI CHARACTER SARA AM
+
+
+	// SKIP SHIFT
+
+	0xBB	0x0E1B #THAI CHARACTER PO PLA
+		+
+	0xD4	0x0E34 #THAI CHARACTER SARA I
+	0xD5	0x0E35 #THAI CHARACTER SARA II
+	0xD6	0x0E36 #THAI CHARACTER SARA UE
+	0xD7	0x0E37 #THAI CHARACTER SARA UEE
+
+
+	// Bug: 'Bud' (Rarely)
+	// อิบัติ <--
+
+	char
+		+
+	0xD4	0x0E34 #THAI CHARACTER SARA I			-4
+		+
+	char											-3
+		+
+	0xEC	0x0E4C #THAI CHARACTER THANTHAKHAT		-2
+		+
+	char											-1
+		+
+	0xD4	0x0E34 #THAI CHARACTER SARA I			-0
 */
 
 unsigned char vowel[] = { 0xd1, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xd3 };
+
+unsigned char bugSaraI[] = { '\n', '\r', '\t' };
+unsigned char SaraAir[] = { 0xD4, 0xD5, 0xD6, 0xD7 };
 
 char* subc(const char* input, int offset, int len, char* dest)
 {
@@ -49,7 +79,8 @@ char* subc(const char* input, int offset, int len, char* dest)
 int shiftLeft(char text[], char color[], int Pos, int shiftBy)
 {
 	int count = 0;
-	while (shiftBy > 0) 
+	
+	while (shiftBy > 0)
 	{
 		text[Pos + 7 - count] = text[Pos - 1 - count];
 		if (!memchr(igor_spaces, text[Pos - 1 - count], sizeof(igor_spaces)))
@@ -58,6 +89,7 @@ int shiftLeft(char text[], char color[], int Pos, int shiftBy)
 		}
 		count++;
 	}
+
 	int tempIndex = Pos - count;
 
 	text[tempIndex] = '{';
@@ -74,7 +106,7 @@ int shiftLeft(char text[], char color[], int Pos, int shiftBy)
 int shiftRight(char text[], char color[], int Pos, int shiftBy)
 {
 	int count = 0;
-	while (shiftBy > 0) 
+	while (shiftBy > 0)
 	{
 		text[Pos - 7 + count] = text[Pos + 1 + count];
 		if (!memchr(igor_spaces, text[Pos + 1 + count], sizeof(igor_spaces)))
@@ -97,13 +129,13 @@ int shiftRight(char text[], char color[], int Pos, int shiftBy)
 	return Pos + count;
 }
 
-int Natives::CE_Convert(AMX *amx, cell *params)
+int Natives::CE_Convert(AMX* amx, cell* params)
 {
 	int
 		len = 0;
 
-	cell *addr = NULL;
-	cell *output = NULL;
+	cell* addr = NULL;
+	cell* output = NULL;
 
 	amx_GetAddr(amx, params[1], &addr);
 	amx_GetAddr(amx, params[2], &output);
@@ -115,38 +147,63 @@ int Natives::CE_Convert(AMX *amx, cell *params)
 		char* text = new char[len];
 		amx_GetString(text, addr, 0, len);
 
-		int 
+		int
 			next_space = -1,
 			last_check = 0,
-			vowel_count = 0
-		;
+			vowel_count = 0,
+			onetime_fix_bud = 0
+			;
 
-		for (int i = 0, j = len; i < j; ++i) 
+		for (int i = 0, j = len; i < j; ++i)
 		{
 			next_space = i + 7;
-			if (text[i] == '{' && next_space < j && text[next_space] == '}') 
+			if (text[i] == '{' && next_space < j && text[next_space] == '}')
 			{
-				char EmbledColor[6]; 
-				subc(text, i+1, 6, EmbledColor);
-				for (int x = last_check + 1; x < i; x++) 
+				char EmbledColor[6];
+				subc(text, i + 1, 6, EmbledColor);
+				for (int x = last_check + 1; x < i; x++)
 				{
-					// find vowel
 					if (memchr(vowel, text[x], sizeof(vowel)))
 					{
-						// Left Shift Color Tag
-						vowel_count++;
 
-						// after vowel (white space, \n, \t)
-						if  (memchr(igor_spaces, text[x - 1], sizeof(igor_spaces)))
+						// ิัิ -44 -47 -44
+						if ((text[x] & 0xD4) == 0xD4 && text[x - 2] == -47 && text[x - 4] == -44 && onetime_fix_bud == 0)
 						{
-							// except some vowel before white space
-							if ((text[x] & 0xd1) == 0 && (text[x+2] & 0xd4) == 0)
+							onetime_fix_bud = 1;
+							continue;
+						}
+
+						if ((text[x] & 0xD4) == 0xD4 && text[x - 1] == -69 && memchr(bugSaraI, text[x + 2], sizeof(bugSaraI)))
+						{
+							vowel_count++;
+							continue;
+						}
+
+						if (memchr(SaraAir, text[x], sizeof(SaraAir)) && text[x - 1] == -69)
+						{
+							continue;
+						}
+
+						/*if ((text[x] & 0xD4) == 0xD4 || (text[x] & 0xD5) == 0xD5 || (text[x] & 0xD6) == 0xD6 || (text[x] & 0xD7) == 0xD7)
+						{
+							if (((char)text[x - 1] & 0xBB) == 0xBB)
+								continue;
+						}*/
+
+						if (memchr(spaces, text[x + 1], sizeof(spaces)) || (text[x + 1] == '{' && memchr(spaces, text[x + 9], sizeof(spaces))))
+						{
+							if ((text[x] & 0xEC) == 0xEC)
 							{
-								// Right Shift Color Tag
-								vowel_count--;
+								continue;
+							}
+
+							if ((text[x] & 0xE9) == 0xE9 && (text[x - 1] & 0xD5) == 0xD5 && (text[x - 2]) == -95) // กี้
+							{
+								continue;
 							}
 						}
 
+						vowel_count++;
 					}
 				}
 				if (vowel_count > 0) shiftLeft(text, EmbledColor, i, vowel_count);
@@ -182,12 +239,12 @@ int Natives::CE_Convert_Dialog(AMX *amx, cell *params)
 		int 
 			next_space = -1,
 			last_check = 0,
-			vowel_count = 0
+			vowel_count = 0,
+			onetime_fix_bud = 0
 		;
 
 		for (int i = 0, j = len; i < j; ++i) 
 		{
-
 			next_space = i + 7;
 			switch(params[3]) // Dialog Style
 			{
@@ -212,28 +269,48 @@ int Natives::CE_Convert_Dialog(AMX *amx, cell *params)
 
 			if (text[i] == '{' && next_space < j && text[next_space] == '}') 
 			{
-
 				char EmbledColor[6];
 				subc(text, i + 1, 6, EmbledColor);
 				for (int x = last_check + 1; x < i; x++) 
 				{
-					// find vowel
 					if (memchr(vowel, text[x], sizeof(vowel)))
 					{
-						// Left Shift Color Tag
-						vowel_count++;
-
-						// after vowel (white space, \n, \t)
-						if  (memchr(igor_spaces, text[x - 1], sizeof(igor_spaces)))
+						// ิัิ -44 -47 -44
+						if ((text[x] & 0xD4) == 0xD4 && text[x - 2] == -47 && text[x - 4] == -44 && onetime_fix_bud == 0)
 						{
-							// except some vowel before white space
-							if ((text[x] & 0xd1) == 0 && (text[x+2] & 0xd4) == 0)
-							{
-								// Right Shift Color Tag
-								vowel_count--;
-							}
+							onetime_fix_bud = 1;
+							continue;
 						}
 
+						if ((text[x] & 0xD4) == 0xD4 && text[x - 1] == -69 && memchr(bugSaraI, text[x + 2], sizeof(bugSaraI)))
+						{
+							vowel_count++;
+							continue;
+						}
+
+						if (memchr(SaraAir, text[x], sizeof(SaraAir)) && text[x - 1] == -69)
+						{
+							continue;
+						}
+
+						/*if ((text[x] & 0xD4) == 0xD4 && (text[x - 2] & 0xEC) == 0xEC && (text[x - 4] & 0xD4) == 0xD4)
+						{
+							continue;
+						}*/
+
+						if (memchr(spaces, text[x + 1], sizeof(spaces)) || (text[x + 1] == '{' && memchr(spaces, text[x + 9], sizeof(spaces))))
+						{
+							if ((text[x] & 0xEC) == 0xEC)
+							{
+								continue;
+							}
+
+							if ((text[x] & 0xE9) == 0xE9 && (text[x - 1] & 0xD5) == 0xD5 && (text[x - 2]) == -95) // กี้
+							{
+								continue;
+							}
+						}
+						vowel_count++;
 					}
 				}
 				if (vowel_count > 0) 
